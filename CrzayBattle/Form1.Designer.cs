da63745f9018a181e0a6bb7027f4aab023612236
RzaHuseyin <rzah1993@gmail.com>
2017-08-20 19:31:19 +0400

    complete game

---
+++ CrzayBattle/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace CrzayBattle
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.SlateGray;
+            this.label2.ForeColor = System.Drawing.Color.MistyRose;
+            this.label2.Location = new System.Drawing.Point(153, 6);
+            this.label2.Name = "label2";
+            this.label2.Padding = new System.Windows.Forms.Padding(6);
+            this.label2.Size = new System.Drawing.Size(119, 36);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Gəmi sayı:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.SlateGray;
+            this.label3.ForeColor = System.Drawing.Color.MistyRose;
+            this.label3.Location = new System.Drawing.Point(262, 6);
+            this.label3.Name = "label3";
+            this.label3.Padding = new System.Windows.Forms.Padding(6);
+            this.label3.Size = new System.Drawing.Size(33, 36);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "7";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.SlateGray;
+            this.label1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label1.ForeColor = System.Drawing.Color.PaleGreen;
+            this.label1.Location = new System.Drawing.Point(25, 6);
+            this.label1.Name = "label1";
+            this.label1.Padding = new System.Windows.Forms.Padding(6);
+            this.label1.Size = new System.Drawing.Size(120, 36);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Yeni Oyun";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.SlateGray;
+            this.label4.ForeColor = System.Drawing.Color.MistyRose;
+            this.label4.Location = new System.Drawing.Point(391, 6);
+            this.label4.MinimumSize = new System.Drawing.Size(55, 36);
+            this.label4.Name = "label4";
+            this.label4.Padding = new System.Windows.Forms.Padding(6);
+            this.label4.Size = new System.Drawing.Size(55, 36);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "0";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.SlateGray;
+            this.label5.ForeColor = System.Drawing.Color.MistyRose;
+            this.label5.Location = new System.Drawing.Point(304, 6);
+            this.label5.Name = "label5";
+            this.label5.Padding = new System.Windows.Forms.Padding(6);
+            this.label5.Size = new System.Drawing.Size(95, 36);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Rekord:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.SlateGray;
+            this.label6.ForeColor = System.Drawing.Color.Azure;
+            this.label6.Location = new System.Drawing.Point(80, 230);
+            this.label6.MinimumSize = new System.Drawing.Size(440, 200);
+            this.label6.Name = "label6";
+            this.label6.Padding = new System.Windows.Forms.Padding(85, 85, 0, 0);
+            this.label6.Size = new System.Drawing.Size(440, 200);
+            this.label6.TabIndex = 6;
+            this.label6.Text = " Qalib  Gəldiniz  XAL:152";
+            this.label6.Visible = false;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.SlateGray;
+            this.label7.ForeColor = System.Drawing.Color.MistyRose;
+            this.label7.Location = new System.Drawing.Point(455, 6);
+            this.label7.Name = "label7";
+            this.label7.Padding = new System.Windows.Forms.Padding(6);
+            this.label7.Size = new System.Drawing.Size(59, 36);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "Xal:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.Color.SlateGray;
+            this.label8.ForeColor = System.Drawing.Color.MistyRose;
+            this.label8.Location = new System.Drawing.Point(508, 6);
+            this.label8.MinimumSize = new System.Drawing.Size(55, 36);
+            this.label8.Name = "label8";
+            this.label8.Padding = new System.Windows.Forms.Padding(6);
+            this.label8.Size = new System.Drawing.Size(55, 36);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "0";
+            this.label8.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(584, 589);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CrazyBattle";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ CrzayBattle/Form1.cs
@@ -0,0 +1,450 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Windows.Forms;
+using System.Media;
+
+namespace CrzayBattle { 
+    public partial class Form1 : Form
+    {
+        List<string> letterList = new List<string>() { "A", "B", "C", "D", "E", "F", "G", "H", "J", "K", "L", "M" };
+        List<string> numbList = new List<string>() { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12" };
+        List<Button> buttonList = new List<Button>();
+        //  Horizontal 
+        Dictionary<string, List<string>> myDic = new Dictionary<string, List<string>>();
+        Dictionary<string, List<string>> ShipDicHorizontal = new Dictionary<string, List<string>>();
+        Dictionary<string, List<string>> ShipDicRemoveHorizontal = new Dictionary<string, List<string>>();
+        Dictionary<string, List<string>> ShipDicHorizontalFull = new Dictionary<string, List<string>>();
+        List<int> valukeysaveList = new List<int>();
+        //   Vertical 
+        Dictionary<string, List<string>> ShipDicVertical = new Dictionary<string, List<string>>();
+        Dictionary<string, List<string>> ShipDicRemoveVertical = new Dictionary<string, List<string>>();
+        Dictionary<string, List<string>> ShipDicVerticalFull = new Dictionary<string, List<string>>();
+
+        List<int> LetterListindex = new List<int>();
+        int count = 0;
+        int clickCount = 0;
+
+        List<string> horizontal = new List<string>();
+        List<string> vertical = new List<string>();
+        Random rm = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            createMethod();
+            createShipListMethot();
+        }
+        public void createMethod()
+        {
+            int top = 90;
+            for (int i = 0; i < 12; i++)
+            {
+                myDic.Add(letterList[i], numbList);
+                int left = 60;
+                for (int j = 0; j < 12; j++)
+                {
+                    Button btn = new Button();
+                    btn.Name = "" + letterList[i] + numbList[j];
+                    btn.Top = top;
+                    btn.Left = left;
+                    btn.Height = 41;
+                    btn.Width = 41;
+                    left += 40;
+                    btn.Click += btnClickMethodHorizontal;
+                    btn.Click += btnClickMethodVertical;
+                    buttonList.Add(btn);
+                    this.Controls.Add(btn);
+                }
+                Label lbl1 = new Label();
+                lbl1.Name = letterList[i];
+                lbl1.Text = lbl1.Name;
+                lbl1.Width = 30;
+                lbl1.Height = 40;
+                lbl1.Top = top + 12;
+                lbl1.Left = 20;
+                this.Controls.Add(lbl1);
+                top += 40;
+                Label lbl2 = new Label();
+                lbl2.Name = numbList[i];
+                lbl2.Text = lbl2.Name;
+                lbl2.Width = 40;
+                lbl2.Height = 40;
+                lbl2.Top = 60;
+                lbl2.Left = top - 67;
+                this.Controls.Add(lbl2);
+            }
+        }
+        public void btnClickMethodHorizontal(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+            ++clickCount;
+            label8.Text = clickCount.ToString();
+            foreach (var item in ShipDicHorizontal)
+            {
+                for (int i = 0; i < item.Value.Count; i++)
+                {
+                    if ((item.Key + item.Value[i]) == btn.Name)
+                    {
+                        btn.Enabled = false;
+                        btn.BackColor = Color.Red;
+                        foreach (var item1 in ShipDicRemoveHorizontal)
+                        {
+                            for (int j = 0; j < item1.Value.Count; j++)
+                            {
+                                if (item1.Key + item1.Value[j] == btn.Name)
+                                {
+                                    if (item1.Value.Count < 2)
+                                    {
+                                        ++count;
+                                        label3.Text = "" + (7 - count);
+                                        if (clickCount > 100)
+                                        {
+                                            string g = "   Məğlub    oldunuz :( ";
+                                            loseWinMsj(g);
+                                        }
+                                        else if (count > 6)
+                                        {
+                                            if (clickCount < Convert.ToInt32(label4.Text) || label4.Text == "0")
+                                            {
+                                                label4.Text = "" + clickCount;
+                                            }
+                                            string g = "Qalib  Gəldiniz XAL:" + clickCount.ToString();
+                                            loseWinMsj(g);
+                                        }
+                                        else
+                                            MessageBox.Show("Gəmi partladıldı", "Xəbərdarlıq");
+                                    }
+                                    else
+                                    {
+                                        item1.Value.Remove(item1.Value[j]);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    else
+                    {
+                        btn.Enabled = false;
+                    }
+                }
+            }
+        }
+        // Horizontal *********************************************
+        public void createShipMethod(int vk, int bg, int hl)
+        {
+            List<string> numbList1 = new List<string>();
+            List<string> numbList2 = new List<string>();
+            for (int i = bg; i < bg + hl; i++)
+            {
+                numbList1.Add(numbList[i]);
+            }
+            for (int i = ((bg > 0) ? (bg - 1) : 0); i < ((bg + hl < 12) ? (bg + hl + 1) : 12); i++)
+            {
+                numbList2.Add(numbList[i]);
+            }
+            ShipDicHorizontal.Add(letterList[vk], numbList1);
+            ShipDicRemoveHorizontal.Add(letterList[vk], numbList1);
+            ShipDicHorizontalFull.Add(letterList[vk], numbList2);
+            for (int i = 0; i < numbList2.Count; i++)
+            {
+                horizontal.Add(letterList[vk] + numbList2[i]);
+                if (vk > 0)
+                    horizontal.Add((letterList[vk - 1] + numbList2[i]));
+                if (vk < 11)
+                    horizontal.Add((letterList[vk + 1] + numbList2[i]));
+            }
+        }
+        public void valuekeysametestMethod(ref int valuekey)
+        {
+            bool z = true;
+            foreach (var item in valukeysaveList)
+            {
+                if ((item == valuekey))
+                {
+                    z = false;
+                    break;
+                }
+            }
+            if (z == false)
+            {
+                valuekey = rm.Next(0, 11);
+                valuekeysametestMethod(ref valuekey);
+            }
+            if (z == true)
+            {
+                valukeysaveList.Add(valuekey);
+                valukeysaveList.Add(valuekey - 1);
+                valukeysaveList.Add(valuekey + 1);
+            }
+        }
+        public void createShipListMethot()
+        {
+            int valuekey = 99;
+            for (int i = 0; i < 4; i++)
+            {
+                valukeysaveList.Add(valuekey);
+                valuekeysametestMethod(ref valuekey);
+
+                int bigining = 12;
+                int howlong = 12;
+                while (bigining + howlong > 12)
+                {
+                    bigining = rm.Next(0, 8);
+                    howlong = rm.Next(3, 6);
+                }
+                createShipMethod(valuekey, bigining, howlong);
+            }
+            createShipListMethotVertical();
+        }
+        // vertical *****************************************
+        public void btnClickMethodVertical(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+            foreach (var item in ShipDicVertical)
+            {
+                for (int i = 0; i < item.Value.Count; i++)
+                {
+                    if ((item.Value[i] + item.Key) == btn.Name)
+                    {
+                        btn.Enabled = false;
+                        btn.BackColor = Color.Red;
+                        foreach (var item1 in ShipDicRemoveVertical)
+                        {
+                            for (int j = 0; j < item1.Value.Count; j++)
+                            {
+                                if (item1.Value[j] + item1.Key == btn.Name)
+                                {
+                                    if (item1.Value.Count < 2)
+                                    {
+                                        ++count;
+                                        label3.Text = "" + (7 - count);
+                                        if (clickCount > 100)
+                                        {
+                                            string g = "   Məğlub    oldunuz)";
+                                            loseWinMsj(g);
+                                        }
+                                        else if (count > 6)
+                                        {
+                                            if (clickCount < Convert.ToInt32(label4.Text) || label4.Text == "0")
+                                            {
+                                                label4.Text = "" + clickCount;
+                                            }
+                                            string g = "Qalib Gəldiniz  XAL:" + clickCount.ToString();
+                                            loseWinMsj(g);
+                                        }
+                                        else
+                                            MessageBox.Show("Gəmi partladıldı", "Xəbərdarlıq");
+                                    }
+                                    else
+                                    {
+                                        item1.Value.Remove(item1.Value[j]);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        public void createShipMethodVertical(int numberStart, int bg, int hl)
+        {
+            List<string> letterList3 = new List<string>();
+            for (int i = bg; i < bg + hl; i++)
+            {
+                letterList3.Add(letterList[i]);
+            }
+            ShipDicVertical.Add(numbList[numberStart], letterList3);
+            ShipDicRemoveVertical.Add(numbList[numberStart], letterList3);
+            for (int i = 0; i < letterList3.Count; i++)
+            {
+                vertical.Add(letterList3[i] + numbList[numberStart]);
+            }
+        }
+        public void numberStartsametestMethodVertical(ref int numberStart)
+        {
+            bool x = true;
+            foreach (var item in LetterListindex)
+            {
+                if (item == numberStart)
+                {
+                    x = false;
+                    break;
+                }
+            }
+            if (x == false)
+            {
+                numberStart = rm.Next(0, 11);
+                numberStartsametestMethodVertical(ref numberStart);
+            }
+            else
+            {
+                LetterListindex.Add(numberStart);
+                LetterListindex.Add(numberStart - 1);
+                LetterListindex.Add(numberStart + 1);
+            }
+        }
+        public void createShipListMethotVertical()
+        {
+            int numberStart = 99;
+            for (int k = 0; k < 3; k++)
+            {
+                LetterListindex.Add(numberStart);
+                numberStartsametestMethodVertical(ref numberStart);
+                int biginingletter = 12;
+                int howlong = 12;
+                while (biginingletter + howlong > 12)
+                {
+                    biginingletter = rm.Next(0, 9);
+                    howlong = rm.Next(2, 5);
+                }
+                createShipMethodVertical(numberStart, biginingletter, howlong);
+            }
+            testVerticalSameHorizontal(ref LetterListindex);
+        }
+        public void testVerticalSameHorizontal(ref List<int> LetterListindex)
+        {
+            bool a = true;
+            foreach (var item in horizontal)
+            {
+                foreach (var item2 in vertical)
+                {
+                    if (item == item2)
+                    {
+                        a = false;
+                        break;
+                    }
+                }
+                if (a == false)
+                {
+                    break;
+                }
+            }
+            if (a == false)
+            {
+                ShipDicVertical.Clear();
+                ShipDicRemoveVertical.Clear();
+                vertical.Clear();
+                LetterListindex.Clear();
+                createShipListMethotVertical();
+            }
+        }
+        public void loseWinMsj(string g)
+        {
+            foreach (var item3 in buttonList)
+            {
+                item3.Enabled = false;
+                item3.BackColor = Color.LightGray;
+            }
+            label6.Text = g;
+            label6.Visible = true;
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 12; i++)
+            {
+                for (int j = 0; j < 12; j++)
+                {
+                    this.Controls.RemoveByKey("" + letterList[i] + numbList[j]);
+
+                }
+            }
+            foreach (var item in letterList)
+            {
+                this.Controls.RemoveByKey("" + item);
+            }
+            foreach (var item in numbList)
+            {
+                this.Controls.RemoveByKey("" + item);
+            }
+            buttonList.Clear();
+            myDic.Clear();
+            count = 0;
+            clickCount = 0;
+            ShipDicHorizontal.Clear();
+            ShipDicRemoveHorizontal.Clear();
+            ShipDicHorizontalFull.Clear();
+            valukeysaveList.Clear();
+            ShipDicVertical.Clear();
+            ShipDicRemoveVertical.Clear();
+            ShipDicVerticalFull.Clear();
+            LetterListindex.Clear();
+            horizontal.Clear();
+            vertical.Clear();
+            label6.Visible = false;
+            label3.Text = "7";
+            label8.Text = "0";
+            createMethod();
+            createShipListMethot();
+        }
+        public void btnClickMethod(Button btn, Dictionary<string, List<string>> ShipDicVertical, Dictionary<string, List<string>> ShipDicRemoveVertical)
+        {
+            foreach (var item in ShipDicVertical)
+            {
+                for (int i = 0; i < item.Value.Count; i++)
+                {
+                    if ((item.Value[i] + item.Key) == btn.Name)
+                    {
+                        btn.Enabled = false;
+                        btn.BackColor = Color.Red;
+                        foreach (var item1 in ShipDicRemoveVertical)
+                        {
+                            for (int j = 0; j < item1.Value.Count; j++)
+                            {
+                                if (item1.Value[j] + item1.Key == btn.Name)
+                                {
+                                    if (item1.Value.Count < 2)
+                                    {
+                                        ++count;
+                                        label3.Text = "" + (7 - count);
+                                        if (clickCount > 100)
+                                        {
+                                            string g = "   Məğlub    oldunuz)";
+                                            SoundPlayer play = new SoundPlayer();
+                                            loseWinMsj(g);
+                                        }
+                                        else if (count > 6)
+                                        {
+                                            if (clickCount < Convert.ToInt32(label4.Text) || label4.Text == "0")
+                                            {
+                                                label4.Text = "" + clickCount;
+                                            }
+                                            string g = "Qalib Gəldiniz  XAL:" + clickCount.ToString();
+                                            loseWinMsj(g);
+                                        }
+                                        else
+                                            MessageBox.Show("Gəmi partladıldı", "Xəbərdarlıq");
+                                    }
+                                    else
+                                    {
+                                        item1.Value.Remove(item1.Value[j]);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
